769912119faccfdfc69640f5450457a0eaccc07e
Simon Blaha <s.blaha@live.at>
2019-02-22 13:42:01 +0100

    install and add firebase database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig: {
+    apiKey: "AIzaSyBwfd2zycOjGXykpD2H7jEYHat1bfLOl9Y",
+    authDomain: "myphonebook-b41ce.firebaseapp.com",
+    databaseURL: "https://myphonebook-b41ce.firebaseio.com",
+    projectId: "myphonebook-b41ce",
+    storageBucket: "myphonebook-b41ce.appspot.com",
+    messagingSenderId: "177752802105"
+  }
 };
 
 /*
