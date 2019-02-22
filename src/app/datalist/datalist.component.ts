afdeedb88b730e8874b939e059a1ee0cca2f30bd
Simon Blaha <s.blaha@live.at>
2019-02-22 20:37:02 +0100

    add wokring content for subpages

--- src/app/datalist/datalist.component.ts
+++ src/app/datalist/datalist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NumberService } from "../shared/number.service";
 
 @Component({
   selector: 'app-datalist',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datalist.component.css']
 })
 export class DatalistComponent implements OnInit {
-
-  constructor() { }
+	numberArray =[];
+  showDeletedMessage : boolean
+  searchText:string = "";
+  constructor(private numberService: NumberService) { }
 
   ngOnInit() {
-  }
+  	 this.numberService.getNumber().subscribe(
+                 (list) => {
+                         this.numberArray = list.map( (item) => {
+                                return {
+                                        $key : item.key,
+                                        ...item.payload.val()
+                                }
+                        })
+                 });
 
 }
+
+onDelete($key){
+     if(confirm("Are you sure you want to delete this record?")){
+       this.numberService.deleteNumber($key);
+       this.showDeletedMessage = true;
+       setTimeout(()=> this.showDeletedMessage=false , 3000)
+     }
+   }
+
+   filterCondition(number){
+     return number.lastName.toLowerCase().indexOf(this.searchText.toLowerCase()) != -1 || number.firstName.toLowerCase().indexOf(this.searchText.toLowerCase()) != -1;
+   }
+ }
--- src/app/importnumbers/importnumbers.component.ts
+++ src/app/importnumbers/importnumbers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NumberService  } from "../shared/number.service";
 
 @Component({
   selector: 'app-importnumbers',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportnumbersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private numberService: NumberService) { }
+	submitted: boolean;
+	formControls = this.numberService.form.controls;
+	showSuccessMessage: boolean;
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	onSubmit(){
+		this.submitted = true;
+		if(this.numberService.form.valid){
+			if(this.numberService.form.get("$key").value == null ){                    
+				this.numberService.insertNumber(this.numberService.form.value);
+				this.showSuccessMessage =true;// we set the property to true
+				} else {
+					this.numberService.updateNumber(this.numberService.form.value);
+					setTimeout(()=> this.showSuccessMessage=false,3000); // we used setTimeout here so after 3 second the showSuccessMessage value will be false
+					this.submitted = false;
+					this.numberService.form.reset();// the form will be empty after new record created
+				//update
+			}
+		}
+	}
 }
